dcf2238d848bcfb96fedb5a404eff88387c8a623
anuteshome <anut15821@gmail.com>
2024-08-04 16:38:18 +0300

    display the elemnts of an array in new ;ine

---
+++ js/project11.js
@@ -0,0 +1,18 @@
+// function list_Array(arr) {
+//     console.log(arr);
+// }
+
+// let arr=[1,"Hello",8,44];
+
+// list_Array(arr);
+
+
+function list_Array(arr) {
+
+    for(let i=0;i<=arr.length-1;i++)
+    console.log(arr[i]);
+}
+
+let arr=[1,"Hello",8,44];
+
+list_Array(arr);
