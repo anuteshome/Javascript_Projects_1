6b0a5e23681fc9ea356eec3f4906f67eeae09e3e
anuteshome <anut15821@gmail.com>
2024-08-04 16:29:40 +0300

    sum of next 10 disgits of the given number

---
+++ js/project10.js
@@ -0,0 +1,12 @@
+function Sum_f(num) {
+    let sum=0;
+    for(let i=num+1;i<=num+10;i++){
+        sum=sum+i;
+       
+    }
+    return sum; 
+}
+
+let sum_result=Sum_f(7);
+console.log(sum_result)
+
